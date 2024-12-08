3037f29caaa8172c11791e81367aed6fffae20c7
Jason <jasonbetsargon@gmail.com>
2024-12-08 14:42:51 -0800

    Initialize project and add project class as module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
+import { Project } from "./projects.js";
 
-console.log(greeting);
+// Create an array to store projects
+let projectsArr = [];
+
+// Create initial default project and add it to array of projects
+const project1 = new Project("Today");
+projectsArr.push(project1);
+console.log(projectsArr);
