aa4eb90120a1238d3d49ef89d8f59e5de882fb9f
Jason <jasonbetsargon@gmail.com>
2024-12-08 20:45:08 -0800

    Initialize page skeleton via render manager constructor

--- src/renderManager.js
+++ src/renderManager.js
@@ -1,14 +1,34 @@
 export class RenderManager {
     constructor() {
         const contentContainer = document.createElement("div");
-        const sideBarContainer = document.createElement("div");
+        const sidebarContainer = document.createElement("div");
         const projectContainer = document.createElement("div");
 
         contentContainer.classList.add("content-container");
-        sideBarContainer.classList.add("sidebar-container");
+        sidebarContainer.classList.add("sidebar-container");
         projectContainer.classList.add("project-container");
 
-        contentContainer.appendChild(sideBarContainer);
+        // Initialize Sidebar Content
+        const addTaskSideContainer = document.createElement("div");
+        addTaskSideContainer.textContent = "add task";
+        const projectsListContainer = document.createElement("div")
+        projectsListContainer.textContent = "Projects";
+
+        sidebarContainer.appendChild(addTaskSideContainer);
+        sidebarContainer.appendChild(projectsListContainer);
+
+        // Initialize Project Content
+        const projectName = document.createElement("h1");
+        projectName.textContent = "Project Name";
+        const tasksContainer = document.createElement("div");
+        const deleteProjButton = document.createElement("button");
+        deleteProjButton.textContent = "Delete Project";
+
+        projectContainer.appendChild(projectName);
+        projectContainer.appendChild(tasksContainer);
+        projectContainer.appendChild(deleteProjButton);
+
+        contentContainer.appendChild(sidebarContainer);
         contentContainer.appendChild(projectContainer);
 
         document.body.appendChild(contentContainer);
