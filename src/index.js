b5886784177d31f3ed1e848150f13d9739a8c9ed
Jason <jasonbetsargon@gmail.com>
2024-12-08 16:39:16 -0800

    Add functionality to project module to add and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { Project } from "./projects.js";
+import { Todo } from "./todo.js";
 
 // Create an array to store projects
 let projectsArr = [];
@@ -7,4 +8,18 @@ let projectsArr = [];
 // Create initial default project and add it to array of projects
 const project1 = new Project("Today");
 projectsArr.push(project1);
+
+// Initialize current/active project (allows to switch focus to other projects)
+const currProject = projectsArr[0];
+
+
+currProject.addTodo("wash dishes");
+currProject.addTodo("run");
+currProject.addTodo("eat");
+currProject.addTodo("sleep", "for at least 8 hours");
+
 console.log(projectsArr);
+console.log(currProject.todos);
+
+currProject.completeTodo(currProject.todos[1]);
+console.log(currProject.todos);
--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,15 @@
 export class Project {
     constructor(name) {
         this.name = name;
+        this.todos = [];
+    }
+
+    addTodo(todoItem) {
+        this.todos.push(todoItem);
+    }
+
+    completeTodo(todoItem) {
+        const index = this.todos.findIndex(task => task === todoItem);
+        this.todos.splice(index, 1);
     }
 }
